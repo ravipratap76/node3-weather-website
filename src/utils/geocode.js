7ee7dfcd86326618530c49c031ecc7c05ee68385
ravipratap76 <ravipratap76@gmail.com>
2024-08-23 15:51:15 +0530

    Init commit

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url ='https://api.mapbox.com/search/geocode/v6/forward?q='+ address +'&access_token=pk.eyJ1IjoicmF2aXByYXRhcDc2IiwiYSI6ImNsenhvY3c1ZTB2MDIycXM4aDQ1N2s4NmgifQ.dYS4f7T4K6Yzj1ILlR5BfA&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -10,9 +10,10 @@ const geocode = (address, callback) => {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
-                latitude: body.features[0].center[1],
-                longitude: body.features[0].center[0],
-                location: body.features[0].place_name
+                
+                latitude: body.features[0].properties.coordinates.latitude,
+                longitude: body.features[0].properties.coordinates.longitude,
+                location: body.features[0].properties.full_address
             })
         }
     })
